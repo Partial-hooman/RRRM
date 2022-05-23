65340d6823b13b455d1ef5d6f3be04c28663637c
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2022-05-23 12:01:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -85,11 +85,11 @@ if f is not None:
          print('created temporary directory', tmpdirname)
          os.chdir(tmpdirname)
          fn = '"' + tfile.name + '"'
-         sp.run(["ffmpeg -i" + " " + {fn} + " " + "-vf "select='eq(pict_type,I)'" -vsync vfr out-%02d.jpeg"], stdin=sp.PIPE, stdout=sp.PIPE, shell=True)
+         sp.run(["ffmpeg", "-i", fn, "-vf", ""select='eq(pict_type,I)'"", "-vsync", "vfr", "out-%02d.jpeg"], stdin=sp.PIPE, stdout=sp.PIPE, shell=True)
          for images in os.listdir(tmpdirname):
  
              # check if the image ends with png
-             if (images.endswith(".jpg")):
+             if (images.endswith(".jpeg")):
                  print(images)
          
       
